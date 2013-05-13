11ba167e466a0a64be850a1f0ee8579b2485747d
Francesco Castagnotto <f_castagnotto@yahoo.it>
2013-05-13 12:40:32 +0200

    Changes on the scripts for cross-compiling the PTS tests Dbench-1.0.0 and Stream-1.1.0.
    Main scripts are:
    
    - "script-dbench.sh" that cross-compile the Dbench test: needs "install-dbench.sh" and "script-pts-xml.sh" in the same folder.
    - "script-stream.sh" that cross-compile the Stream test: needs "install-stream.sh" and "script-pts-xml.sh" in the same folder.

--- contrib/thesis-fcastagnotto/script-stream.sh
+++ contrib/thesis-fcastagnotto/script-stream.sh
@@ -1,11 +1,14 @@
 ######################################################################################
-#   STREAM-1.0.0								     #
+#   STREAM-1.1.0								     #
 ######################################################################################
-#This script run the download of the source of the Phoronix test Stream-1.0.0
+
+#This script run the download of the source of the Phoronix test Stream-1.1.0
 #and the cross compilation to functioning on a ARM linux architecture.
-#It require the test files (that can be downloaded on the target by the Phoronix Suit
-#by the command "phoronix-test-suite download-test-files pts/stream") in the
-#same folder of this script.
+
+#It require the ZIP test file "stream-1.1.0.zip" in the same folder of this script.
+#(The file can be downloaded on the target using the Phoronix Suite by the command
+#"phoronix-test-suite download-test-files pts/stream", and then can be found inside
+#the folder "$HOME/.phoronix-test-suite/openbenchmarking.org/pts/")
 
 #!/bin/bash
 
