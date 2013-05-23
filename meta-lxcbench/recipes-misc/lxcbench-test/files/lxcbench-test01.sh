59694bfbccfbdf8ad4176846eee10e3893a72561
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2013-05-23 08:37:30 +0200

    lxcbench-test01.sh: Use systemwide installed PTS if available

--- meta-lxcbench/recipes-misc/lxcbench-test/files/lxcbench-test01.sh
+++ meta-lxcbench/recipes-misc/lxcbench-test/files/lxcbench-test01.sh
@@ -3,7 +3,9 @@
 # ==================================================================================
 # Project: 	LXCBENCH
 #
-# Description:	lxcbench-test01
+# Description:	LXCBENCH Test Suite #1
+#
+# See http://projects.genivi.org/lxcbench
 # ==================================================================================
 
 #set -x
@@ -11,7 +13,7 @@ set -e
 
 PTS_TARBALL=phoronix-test-suite-4.4.1.tar.gz
 PTS_INSTALLDIR=~/phoronix-test-suite
-PTS_WORKDIR=~/.phoronix-test-suite
+PTS_USERDIR=~/.phoronix-test-suite
 
 PROGNAME=`basename $0`
 echo "INFO: ${PROGNAME} v1.1"
@@ -26,9 +28,15 @@ fi
 # Manually Install PTS from .tar.gz
 # -----------------------------------------------------------------------------------
 
-if [ ! -x "${PTS_INSTALLDIR}/phoronix-test-suite" ]; then
+PTS_EXE=`which phoronix-test-suite`
+if [ "${PTS_EXE}" != "" ]; then
+    PTS_INSTALLDIR="`dirname ${PTS_EXE}`"
+    PTS_EXE="`basename ${PTS_EXE}`"
+    echo "INFO: Using system-installed ${PTS_EXE}"
+fi
 
-    echo "INFO: Installing ${PTS_TARBALL}"
+if [ ! -x "${PTS_INSTALLDIR}/phoronix-test-suite" ]; then
+    echo "INFO: Installing ${PTS_TARBALL} into ${PTS_INSTALLDIR}"
 
     # Manually install PTS dependencies
     which apt-get && apt-get install php5-cli php5-curl php-fpdf
@@ -40,6 +48,7 @@ if [ ! -x "${PTS_INSTALLDIR}/phoronix-test-suite" ]; then
 	http://www.phoronix.net/downloads/phoronix-test-suite/releases/${PTS_TARBALL} \
 	| tar xvz -C ${PTS_INSTALLDIR}/..
 
+    PTS_EXE="./phoronix-test-suite"
 fi
 
 # -----------------------------------------------------------------------------------
@@ -48,26 +57,26 @@ fi
 
 mkdir -p ${PTS_INSTALLDIR}
 cd ${PTS_INSTALLDIR}
-./phoronix-test-suite <<EOT
+${PTS_EXE} <<EOT
 Y
 Y
 Y
 EOT
 
 # -----------------------------------------------------------------------------------
-# Perform LXCBENCH tests on Ubuntu
+# Perform LXCBENCH tests
 # -----------------------------------------------------------------------------------
 
 cd ${PTS_INSTALLDIR}
 
 ## Install dependencies for PTS tests
-./phoronix-test-suite install-dependencies pts/cachebench pts/dbench pts/c-ray pts/encode-mp3 pts/stream
+${PTS_EXE} install-dependencies pts/cachebench pts/dbench pts/c-ray pts/encode-mp3 pts/stream
 
 ## Download and install the tests
-./phoronix-test-suite install pts/cachebench pts/dbench pts/c-ray pts/encode-mp3 pts/stream
+${PTS_EXE} install pts/cachebench pts/dbench pts/c-ray pts/encode-mp3 pts/stream
 
 ## Configure Batch Mode
-./phoronix-test-suite batch-setup <<EOT
+${PTS_EXE} batch-setup <<EOT
 y
 n
 n
@@ -78,13 +87,13 @@ y
 EOT
 
 ## Run Batch Mode
-./phoronix-test-suite batch-run pts/cachebench pts/dbench pts/c-ray pts/encode-mp3 pts/stream
+${PTS_EXE} batch-run pts/cachebench pts/dbench pts/c-ray pts/encode-mp3 pts/stream
 
 # -----------------------------------------------------------------------------------
 # Look at test results
 # -----------------------------------------------------------------------------------
 
-ls -la ${PTS_WORKDIR}/test-results/
+ls -la ${PTS_USERDIR}/test-results/
 
 
 # === EOF ===
